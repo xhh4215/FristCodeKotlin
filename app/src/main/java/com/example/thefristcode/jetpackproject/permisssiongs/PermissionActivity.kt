6bf33154a323f0fbdfd5f518c3515d7c0731eeef
xhh <18734184215@163.com>
2020-09-02 18:08:51 +0800

    通过ContentProvider获取联系人信息

--- app/src/main/java/com/example/thefristcode/jetpackproject/permisssiongs/PermissionActivity.kt
+++ app/src/main/java/com/example/thefristcode/jetpackproject/permisssiongs/PermissionActivity.kt
@@ -6,6 +6,8 @@ import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -14,23 +16,25 @@ import kotlinx.android.synthetic.main.activity_permission.*
 import java.util.jar.Manifest
 
 class PermissionActivity : AppCompatActivity() {
+    private val contactList = ArrayList<String>()
+    private lateinit var adapter: ArrayAdapter<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_permission)
-        btnCall.setOnClickListener {
-            if (ContextCompat.checkSelfPermission(
-                    this,
-                    android.Manifest.permission.CALL_PHONE
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(android.Manifest.permission.CALL_PHONE),
-                    1
-                )
-            } else {
-                call()
-            }
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, contactList)
+        contartList.adapter = adapter
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.READ_CONTACTS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.READ_CONTACTS),
+                1
+            )
+        } else {
+            getContactList()
         }
     }
 
@@ -43,7 +47,7 @@ class PermissionActivity : AppCompatActivity() {
         when (requestCode) {
             1 -> {
                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    call()
+                    getContactList()
                 } else {
                     Toast.makeText(this, "no permission", Toast.LENGTH_SHORT).show()
                 }
@@ -51,11 +55,25 @@ class PermissionActivity : AppCompatActivity() {
         }
     }
 
-    fun call() {
-        Intent(Intent.ACTION_CALL)
-            .apply {
-                this.data = Uri.parse("tel:10086")
-                startActivity(this)
+
+    fun getContactList() {
+        contentResolver.query(
+            ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+            , null, null, null, null
+        )
+            ?.apply {
+                while (moveToNext()) {
+                    val displayname =
+                        getString(getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
+                    val number =
+                        getString(getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                    contactList.add("$displayname\n$number")
+
+                }
+                adapter
+                    .notifyDataSetChanged()
+                close()
             }
     }
+
 }

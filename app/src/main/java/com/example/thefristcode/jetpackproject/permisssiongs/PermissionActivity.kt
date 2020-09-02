659a2d40c5f03c50637dac2f7ac6468a237fe09f
xhh <18734184215@163.com>
2020-09-02 17:10:38 +0800

    android 的动态的运行时权限学习使用

---
+++ app/src/main/java/com/example/thefristcode/jetpackproject/permisssiongs/PermissionActivity.kt
@@ -0,0 +1,61 @@
+package com.example.thefristcode.jetpackproject.permisssiongs
+
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.thefristcode.jetpackproject.R
+import kotlinx.android.synthetic.main.activity_permission.*
+import java.util.jar.Manifest
+
+class PermissionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_permission)
+        btnCall.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.CALL_PHONE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.CALL_PHONE),
+                    1
+                )
+            } else {
+                call()
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    call()
+                } else {
+                    Toast.makeText(this, "no permission", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    fun call() {
+        Intent(Intent.ACTION_CALL)
+            .apply {
+                this.data = Uri.parse("tel:10086")
+                startActivity(this)
+            }
+    }
+}
